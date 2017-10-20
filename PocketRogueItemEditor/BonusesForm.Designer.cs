3ebfd5bb4c320467bb1877316741aab276a95d70
Daron Arakelian <dha42@drexel.edu>
2017-10-20 16:33:21 -0400

    Final v1.0.0 version of editor

---
+++ PocketRogueItemEditor/BonusesForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PocketRogueItemEditor
+{
+    partial class BonusesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.updateBonusesButton = new System.Windows.Forms.Button();
+            this.bonusesGridView = new System.Windows.Forms.DataGridView();
+            this.BonusColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemDefinitionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bonusesGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemDefinitionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // updateBonusesButton
+            // 
+            this.updateBonusesButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.updateBonusesButton.Location = new System.Drawing.Point(0, 295);
+            this.updateBonusesButton.Name = "updateBonusesButton";
+            this.updateBonusesButton.Size = new System.Drawing.Size(284, 37);
+            this.updateBonusesButton.TabIndex = 0;
+            this.updateBonusesButton.Text = "Update";
+            this.updateBonusesButton.UseVisualStyleBackColor = true;
+            this.updateBonusesButton.Click += new System.EventHandler(this.updateBonusesButton_Click);
+            // 
+            // bonusesGridView
+            // 
+            this.bonusesGridView.AllowUserToAddRows = false;
+            this.bonusesGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.bonusesGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.BonusColumn,
+            this.ValueColumn});
+            this.bonusesGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bonusesGridView.Location = new System.Drawing.Point(0, 0);
+            this.bonusesGridView.Name = "bonusesGridView";
+            this.bonusesGridView.RowHeadersVisible = false;
+            this.bonusesGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.bonusesGridView.Size = new System.Drawing.Size(284, 295);
+            this.bonusesGridView.TabIndex = 1;
+            // 
+            // BonusColumn
+            // 
+            this.BonusColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.BonusColumn.HeaderText = "Bonus";
+            this.BonusColumn.Name = "BonusColumn";
+            // 
+            // ValueColumn
+            // 
+            this.ValueColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.ValueColumn.HeaderText = "Value";
+            this.ValueColumn.Name = "ValueColumn";
+            // 
+            // itemDefinitionBindingSource
+            // 
+            this.itemDefinitionBindingSource.DataSource = typeof(PocketRogueItemEditor.ItemDefinition);
+            // 
+            // BonusesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 332);
+            this.Controls.Add(this.bonusesGridView);
+            this.Controls.Add(this.updateBonusesButton);
+            this.Name = "BonusesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "BonusesForm";
+            ((System.ComponentModel.ISupportInitialize)(this.bonusesGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.itemDefinitionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateBonusesButton;
+        private System.Windows.Forms.DataGridView bonusesGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BonusColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValueColumn;
+        private System.Windows.Forms.BindingSource itemDefinitionBindingSource;
+    }
+}
---
+++ PocketRogueItemEditor/BonusesForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PocketRogueItemEditor
+{
+    public partial class BonusesForm : Form
+    {
+        private ItemDefinition itemDefinition;
+
+        public BonusesForm(ItemDefinition itemDefinition)
+        {
+            InitializeComponent();
+            this.itemDefinition = itemDefinition;
+            int[] bonuses = this.itemDefinition.bonuses;
+
+            //Add rows
+            bonusesGridView.Rows.Add(new object[] {"Melee Damage", bonuses[0] });
+            bonusesGridView.Rows.Add(new object[] { "Magic Damage", bonuses[1] });
+            bonusesGridView.Rows.Add(new object[] { "Range Damage", bonuses[2] });
+            bonusesGridView.Rows.Add(new object[] { "Attack Speed", bonuses[3] });
+            bonusesGridView.Rows.Add(new object[] { "Melee Defense", bonuses[4] });
+            bonusesGridView.Rows.Add(new object[] { "Magic Defense", bonuses[5] });
+            bonusesGridView.Rows.Add(new object[] { "Range Defense", bonuses[6] });
+        }
+
+        private void updateBonusesButton_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach (DataGridViewRow bonusRow in this.bonusesGridView.Rows)
+            {
+                this.itemDefinition.bonuses[count] = Convert.ToInt32(bonusRow.Cells[1].Value);
+                count++;
+            }
+            this.Close();
+        }
+    }
+}
